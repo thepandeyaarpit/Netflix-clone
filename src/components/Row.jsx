0b055e8a4921a0723c3f558ea171df735a8edcfb
Arpit <69616138+thepandeyaarpit@users.noreply.github.com>
2022-05-10 18:13:08 +0530

    update

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,11 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-
-
-const Row = ({title, fetchURL}) => {
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Movie from './Movie';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+const Row = ({title, fetchURL, rowID}) => {
 
     const [movies, setMovies] = useState([])
+    const [like, setLike] = useState(false)
 
     useEffect(()=>{
         axios.get(fetchURL).then((response) => {
@@ -12,20 +13,30 @@ const Row = ({title, fetchURL}) => {
         })
     },[fetchURL])
 
+    const slideLeft = () => {
+        var slider = document.getElementById('slider' + rowID)
+        slider.scrollLeft = slider.scrollLeft - 500;
+    }
+
+    const slideRight = () => {
+        var slider = document.getElementById('slider' + rowID)
+        slider.scrollLeft = slider.scrollLeft + 500;
+    }
+
   return (
     <>
         <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-        <div className='relative flex items-center'>
-            <div id={'slider'}>
-                {movies.map((item, id) => {
-                    <div className='w-[160px] sm:w[200px] md:w-[240px] lg:w[280px] inline-block cursor-pointer relative p-2'>
-                        <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
-                    </div>
-                })}
+        <div className='relative flex items-center group'>
+            <MdChevronLeft onClick={slideLeft} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
+            <div id={'slider' + rowID} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+                {movies.map((item, id) => (
+                    <Movie key={id} item={item} />
+                ))}
             </div>
+            <MdChevronRight onClick={slideRight} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
         </div>
     </>
   )
 }
 
-export default Row
+export default Row;
