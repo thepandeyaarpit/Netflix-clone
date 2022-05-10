0e3189ee6c8b2498a0bbbddf05f74cbffcab34c6
Arpit <69616138+thepandeyaarpit@users.noreply.github.com>
2022-05-10 14:46:59 +0530

    change

---
+++ src/Request.js
@@ -0,0 +1,10 @@
+const key = 'c68a1ae6cf4bf26f51ac572b17ea3fe7'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+export default requests;
