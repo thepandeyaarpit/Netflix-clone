1a120feee2d621a79dc5e301d99351876032dcfe
Arpit <69616138+thepandeyaarpit@users.noreply.github.com>
2022-05-10 14:46:45 +0530

    update

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+    const [movies, setMovies] = useState([])
+
+
+  return (
+    <div>Main</div>
+  )
+}
+
+export default Main
+// c68a1ae6cf4bf26f51ac572b17ea3fe7
