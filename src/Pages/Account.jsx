ff42be34c781fda8ea87819ee1e7cd7e21e7ab97
Arpit <69616138+thepandeyaarpit@users.noreply.github.com>
2022-05-11 15:47:32 +0530

    update

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import SavedShows from '../components/SavedShows';
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <>
+      <div className='w-full text-white'>
+      <img className='w-full h-[400px] object-cover' 
+        src='https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/8e07a611-4ced-4d4e-81e6-d278862c952b/IN-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg' 
+        alt='/' />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+        <div className='absolute top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
   )
 }
 
-export default Account
+export default Account;
