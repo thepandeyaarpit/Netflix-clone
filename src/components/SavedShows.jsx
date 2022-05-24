ff46de36ddf608559c0ccc38893cf6107ea7a321
Arpit <69616138+thepandeyaarpit@users.noreply.github.com>
2022-05-24 17:37:27 +0530

    chnages

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -45,8 +45,8 @@ const SavedShows = () => {
         <div className='relative flex items-center group'>
             <MdChevronLeft onClick={slideLeft} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
             <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
-                {movies.map((item, id) => (
-                    <div key={id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+                {movies.map((item) => (
+                    <div key={item.id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
                         <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.img}`} alt={item?.title} />
                         <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
                             <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
